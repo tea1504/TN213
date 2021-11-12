a4f448f794d87ca3415996717974066b51d4b85b
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-12 20:56:15 +0700

    Thêm DangNhap cho LoginController

--- Website/Controllers/LoginController.cs
+++ Website/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Models;
 
 namespace Website.Controllers
 {
@@ -12,6 +13,13 @@ namespace Website.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost, ActionName("Index")]
+        public ActionResult DangNhap(LoginModel login)
+        {
+            ModelState.AddModelError("", "Lỗi");
+            return View("Index", login);
         }
     }
 }

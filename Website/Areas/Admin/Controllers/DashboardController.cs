b5937e32fdc0889cf6c5e79ba0361b12f5ed0d8b
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-12 21:53:03 +0700

    Tạo CheckLoginController

--- Website/Areas/Admin/Controllers/DashboardController.cs
+++ Website/Areas/Admin/Controllers/DashboardController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Controllers;
 
 namespace Website.Areas.Admin.Controllers
 {
-    public class DashboardController : Controller
+    public class DashboardController : CheckLoginController
     {
         // GET: Admin/Dashboard
         public ActionResult Index()

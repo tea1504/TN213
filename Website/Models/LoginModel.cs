616657dbef4f6046375a8860361c4633f8c5c8d2
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-12 20:36:22 +0700

    Tạo view Login/Index

--- Website/Models/LoginModel.cs
+++ Website/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace Website.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Bạn phải nhận tài khoản")]
+        [StringLength(50)]
+        public string taikhoan { get; set; }
+
+        [Required(ErrorMessage = "Bạn phải nhập mật khẩu")]
+        [StringLength(100)]
+        public string matkhau { get; set; }
     }
 }

9390116c2300625562b894d32baae6d93227c430
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-13 09:31:38 +0700

    Tạo FilterNhaTroModel

---
+++ Website/Models/FilterNhaTroModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class FilterNhaTroModel
+    {
+        int? ma_kv { set; get; }
+        float? tiendien { set; get; }
+        float? tiennuoc { set; get; }
+        float? giaphong { set; get; }
+    }
+}

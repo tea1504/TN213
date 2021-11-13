37203c87b3c2fd25aae092cd9ec15810a013d5e3
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-13 20:45:05 +0700

    Tạo DanhGiaDAO

---
+++ Website/DAO/DanhGiaDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Website.EF;
+
+namespace Website.DAO
+{
+    public class DanhGiaDAO
+    {
+        NhaTroDBContext db = null;
+        public DanhGiaDAO()
+        {
+            db = new NhaTroDBContext();
+        }
+
+    }
+}

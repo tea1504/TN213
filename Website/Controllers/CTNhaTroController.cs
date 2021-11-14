83ae86012074cab86b6b9136c9a39ccc0fe5a74e
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-14 10:50:04 +0700

    Tao CTNhaTroController

---
+++ Website/Controllers/CTNhaTroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class CTNhaTroController : Controller
+    {
+        // GET: CTNhaTro
+        public ActionResult Index(int ma_nt)
+        {
+            return View();
+        }
+    }
+}

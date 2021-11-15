b82dcb5c0b1963e27a8fe2f66370aa5322f16477
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-15 21:43:23 +0700

    Tạo TruongHocJsonModel

--- Website/Controllers/SchoolController.cs
+++ Website/Controllers/SchoolController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.DAO;
+using Website.EF;
 
 namespace Website.Controllers
 {
@@ -12,8 +14,14 @@ namespace Website.Controllers
         {
             if (ma_th == null)
             {
-                return Json(data, JsonRequestBehavior.AllowGet);
+                return Json("", JsonRequestBehavior.AllowGet);
             }
+            if(ma_th == 0)
+            {
+                var d = new TruongHocDAO().GetAllTruongHoc();
+                return Json(d, JsonRequestBehavior.AllowGet);
+            }
+            var data = new TruongHocDAO().GetTruongHoc(ma_th ?? 1);
             return Json(data, JsonRequestBehavior.AllowGet);
         }
     }

fe935bb00d6a9287f78bd9f7a62f226cdc7ed48e
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-15 21:35:03 +0700

    Tạo TruongHocDAO/GetAllTruongHoc

--- Website/DAO/TruongHocDAO.cs
+++ Website/DAO/TruongHocDAO.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Website.EF;
 
 namespace Website.DAO
 {
     public class TruongHocDAO
     {
+        NhaTroDBContext db = null;
+        public TruongHocDAO()
+        {
+            db = new NhaTroDBContext();
+        }
+        public List<TruongHoc> GetAllTruongHoc()
+        {
+            var res = db.TruongHocs.ToList();
+            return res;
+        }
     }
 }

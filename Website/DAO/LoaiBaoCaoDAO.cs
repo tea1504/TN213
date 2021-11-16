ec306e72078c42cdcefe2269818263296114ae98
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-16 14:03:33 +0700

    Tạo LoaiBaoCaoDAO/GetAllLoaiBaoCao

--- Website/DAO/LoaiBaoCaoDAO.cs
+++ Website/DAO/LoaiBaoCaoDAO.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Website.EF;
 
 namespace Website.DAO
 {
     public class LoaiBaoCaoDAO
     {
+        NhaTroDBContext db = null;
+        public LoaiBaoCaoDAO()
+        {
+            db = new NhaTroDBContext();
+        }
+        public List<LoaiBaoCao> GetAllLoaiBaoCao()
+        {
+            var res = db.LoaiBaoCaos.ToList();
+            return res;
+        }
     }
 }

d7ed0e61a44504481e6db71d06395f6d589a770a
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-16 14:01:05 +0700

    Tao contructor BaoCaoNguoiDungDAO

--- Website/DAO/BaoCaoNguoiDungDAO.cs
+++ Website/DAO/BaoCaoNguoiDungDAO.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Website.EF;
 
 namespace Website.DAO
 {
     public class BaoCaoNguoiDungDAO
     {
+        NhaTroDBContext db = null;
+        public BaoCaoNguoiDungDAO()
+        {
+            db = new NhaTroDBContext();
+        }
     }
 }

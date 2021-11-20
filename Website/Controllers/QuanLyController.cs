90ca83e2f8602e1c7e6c5c2259155783cad104b7
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-11-20 19:35:53 +0700

    Tạo QuanLyController/NhaTro

--- Website/Controllers/QuanLyController.cs
+++ Website/Controllers/QuanLyController.cs
@@ -5,16 +5,25 @@ using System.Web;
 using System.Web.Mvc;
 using Website.DAO;
 using Website.EF;
+using Website.Models;
 
 namespace Website.Controllers
 {
     public class QuanLyController : CheckUserController
     {
         // GET: QuanLy
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            List<NhaTro> res = new NhaTroDAO()
-            return View();
+            UserLoginModel user = (UserLoginModel)Session["USER_LOGIN"];
+            List<NhaTro> res = new NhaTroDAO().GetNhaTroTheoChuTro(user.ma_nd);
+            ViewBag.page = page;
+            ViewBag.max = (int)(res.Count() / 5) + ((res.Count() % 5 != 0) ? 1 : 0);
+            return View(res.Skip(5 * (page - 1)).Take(5));
+        }
+        public ActionResult NhaTro(int id)
+        {
+            NhaTro res = new NhaTroDAO().GetNhaTro(id);
+            return View(res);
         }
     }
 }

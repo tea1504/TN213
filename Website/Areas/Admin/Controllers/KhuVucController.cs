bc0e2ab91b27f05245b117b4116da88add455361
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-12-03 20:02:38 +0700

    Tạo Khu Vực

---
+++ Website/Areas/Admin/Controllers/KhuVucController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Controllers;
+
+namespace Website.Areas.Admin.Controllers
+{
+    public class KhuVucController : CheckAdminController
+    {
+        // GET: Admin/KhuVuc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

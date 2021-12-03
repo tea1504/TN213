f3ed5c582c89917d2e86000e5aa4f509179716da
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-12-03 21:34:02 +0700

    Tạo chức năng thêm mới khu vuc

--- Website/Areas/Admin/Controllers/KhuVucController.cs
+++ Website/Areas/Admin/Controllers/KhuVucController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Website.Controllers;
 using Website.DAO;
+using Website.EF;
 
 namespace Website.Areas.Admin.Controllers
 {
@@ -15,6 +16,21 @@ namespace Website.Areas.Admin.Controllers
         {
             var data = new KhuVucDAO().GetAllKhuVuc();
             return View(data);
+        }
+        public JsonResult GetKhuVuc(int id)
+        {
+            KhuVuc kv = new KhuVucDAO().LayKhuVuc(id);
+            var data = new
+            {
+                latlng = kv.toado_kv.AsText(),
+                polygon = kv.polygon_kv.AsText(),
+                mau = kv.ma_ms,
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Create()
+        {
+            return View();
         }
     }
 }
--- Website/Scripts/js/AdminKhuVucScript.js
+++ Website/Scripts/js/AdminKhuVucScript.js
@@ -1,4 +1,52 @@
-﻿$(document).ready(function () {
+﻿function getLatLng(arr) {
+    let res = [];
+    arr.map((item, ind) => {
+        ind % 2 == 0 && res.push([item]);
+        ind % 2 == 1 && res[res.length - 1].unshift(item);
+    });
+    return res;
+}
+
+$(document).ready(function () {
+
+    var map = L.map("map", { center: [10.032778, 105.759444], zoom: 14 });
+
+    L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+            attribution: '&copy; <a href="http://' +
+                'www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }
+    ).addTo(map);
+
+    var khuVucLayer = L.layerGroup().addTo(map);
+
+    $('.show-map').click(e => {
+        setTimeout(function () {
+            map.invalidateSize();
+        }, 50);
+        var element = e.delegateTarget;
+        var id = element.getAttribute('data-id');
+        khuVucLayer.clearLayers();
+        $.ajax({
+            url: '/Admin/KhuVuc/GetKhuVuc/' + id,
+            type: 'post',
+            success: res => {
+                var coors = res.polygon.match(/[0-9]+\.*[0-9]*/ig);
+                var layer = L.polygon(getLatLng(coors)).addTo(khuVucLayer);
+                layer.setStyle({
+                    color: '#' + res.mau,
+                    fillColor: '#' + res.mau,
+                })
+                coors = res.latlng.match(/[0-9]+\.*[0-9]*/ig);
+                map.setView([coors[1], coors[0]], 15);
+            },
+            error: err => {
+
+            }
+        })
+    })
+
     $('#myTable').DataTable();
     $('[data-toggle="tooltip"]').tooltip()
 })

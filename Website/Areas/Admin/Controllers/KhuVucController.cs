0ce12d02315dd42cba42254ac04c8daddb128b82
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-12-03 20:19:03 +0700

    Thêm AdminKhuVucScript

--- Website/Areas/Admin/Controllers/KhuVucController.cs
+++ Website/Areas/Admin/Controllers/KhuVucController.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Website.Controllers;
+using Website.DAO;
 
 namespace Website.Areas.Admin.Controllers
 {
-    public class KhuVucController : CheckAdminController
+    public class KhuVucController : Controller
     {
         // GET: Admin/KhuVuc
         public ActionResult Index()
         {
-            return View();
+            var data = new KhuVucDAO().GetAllKhuVuc();
+            return View(data);
         }
     }
 }

e3c1277be39c5a37fd2c459e765dab8982e2d869
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-12-03 22:11:57 +0700

    Chức năng preview màu sắc

--- Website/Scripts/js/AdminKhuVucCreateScript.js
+++ Website/Scripts/js/AdminKhuVucCreateScript.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function () {
+    $('#ma_ms').change(e => {
+        var ma = $('#ma_ms').val();
+        if (!ma)
+            $('#preview-color').css('background', 'gray')
+        else
+            $('#preview-color').css('background', '#' + ma);
+    })
+})

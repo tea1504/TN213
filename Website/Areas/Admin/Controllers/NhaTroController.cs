734f39a72ace848b501f147122cf3e2b75c854da
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-12-04 19:19:11 +0700

    Tạo Quản lý nhà trọ

---
+++ Website/Areas/Admin/Controllers/NhaTroController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.DAO;
+using Website.EF;
+
+namespace Website.Areas.Admin.Controllers
+{
+    public class NhaTroController : Controller
+    {
+        // GET: Admin/NhaTro
+        public ActionResult Index()
+        {
+            List<NhaTro> model = new NhaTroDAO().GetAllNhaTro();
+            return View(model);
+        }
+    }
+}

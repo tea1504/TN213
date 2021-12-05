d17a304a8ee344576c280dc7a5296342b1674173
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-12-06 01:24:48 +0700

    Hoàn thành

--- Website/DAO/MauSacDAO.cs
+++ Website/DAO/MauSacDAO.cs
@@ -18,5 +18,29 @@ namespace Website.DAO
             var res = db.MauSacs.ToList();
             return res;
         }
+        public MauSac Get(string ma_ms)
+        {
+            var res = db.MauSacs.Find(ma_ms);
+            return res;
+        }
+        public MauSac Add(MauSac ms)
+        {
+            var res = db.MauSacs.Add(ms);
+            db.SaveChanges();
+            return res;
+        }
+        public MauSac Edit(MauSac ms)
+        {
+            var res = db.MauSacs.Find(ms.ma_ms);
+            res.ten_ms = ms.ten_ms;
+            db.SaveChanges();
+            return res;
+        }
+        public void Delete(string ma_ms)
+        {
+            var res = db.MauSacs.Find(ma_ms);
+            db.MauSacs.Remove(res);
+            db.SaveChanges();
+        }
     }
 }
---
+++ Website/Scripts/js/AdminMauSacScript.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    get_color.addEventListener("change", watchColorPicker, false);
+
+    function watchColorPicker(e) {
+        var color = e.target.value;
+        $('#ma_ms').val(color.substring(1));
+        $('#ten_ms').val(color);
+    }
+
+    $('.btnedit').click(e => {
+        var el = e.delegateTarget;
+        var ma = el.getAttribute('data-id');
+        var name = el.getAttribute('data-name');
+        $('#exampleModalLabel2').html('Chỉnh sửa màu ' + name);
+        $('#ma_ms_edit').val(ma);
+        $('#ma_ms_edit').css('background', '#' + ma);
+        $('#ten_ms_edit').val(name);
+    })
+
+    $('.btndelete').click(e => {
+        var el = e.delegateTarget;
+        var ma = el.getAttribute('data-id');
+        var name = el.getAttribute('data-name');
+        var sl = el.getAttribute('data-sl');
+        if (sl > 0) {
+            Swal.fire('Không thể xóa', 'Màu ' + name + ' đang được dùng bạn không thể xóa', 'info');
+        }
+        else {
+            Swal.fire({
+                title: 'Bạn có chắc chắn muốn xóa khu vực ' + name + '?',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'Xóa',
+                denyButtonText: `Không xóa`,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = '/Admin/MauSac/Delete/' + ma;
+                } else if (result.isDenied) {
+                    Swal.fire('Đã hủy thao tác', '', 'info');
+                }
+            });
+        }
+    })
+
+    $('[data-toggle="tooltip"]').tooltip();
+    $('#myTable').DataTable();
+});
